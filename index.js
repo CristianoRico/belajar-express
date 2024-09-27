f0beff6aee47bd1aec18425c875ca0d88445d997
CristianoRico <carvalhocristianorico@gmail.com>
2024-09-27 11:18:14 +0700

    membuat route prodi dan berita

--- index.js
+++ index.js
@@ -2,22 +2,41 @@ const express = require('express') // impor modul express
 const app = express()// inisialisasi express
 const port = 3000 // port
 
+app.set('view engine', 'ejs');
+
 // route /
 app.get('/', (req, res) => {
     // res.send('Hello');
-    res.sendFile(__dirname + '/home.html');
+    // res.sendFile(__dirname + '/home.html');
+
+    const berita = [
+        {
+            judul: 'Berita 1',
+            isi: 'Isi Berita 1'
+        },
+        {
+            judul: 'Berita 2',
+            isi: 'Isi Berita 2'
+        },
+    ];
+
+    res.render('index', {title : 'Halaman Home', berita});
+
 });
+
 
 // route /about
 app.get('/about', (req, res) => {
     // res.send('About Us');
-    res.sendFile(__dirname + '/about.html');
+    // res.sendFile(__dirname + '/about.html');
+    res.render('about');
 });
 
 // route /contact
 app.get('/contact', (req, res) => {
     // res.send('Contact Us');
-    res.sendFile(__dirname + '/contact.html');
+    // res.sendFile(__dirname + '/contact.html');
+    res.render('contact');
 });
 
 // route /mahasiswa
@@ -44,13 +63,52 @@ app.get('/dosen', (req, res) => {
             {
                 prodi : 'Informatika', 
                 nama : ['Derry', 'Siska' , 'Yohannes']
-            },
+            }
         ]
     })
 });
+
+// route /prodi
+app.get('/prodi', (req, res) => {
+    
+    const prodi = [
+        {
+            prodi : 'Sistem Informasi',
+            fakultas : 'FIKR',
+            singkatan :'SI'
+        },
+        {
+            prodi : 'Informatika',
+            fakultas : 'FIKR',
+            singkatan :'IF'
+        },
+        {
+            prodi : 'Teknik Elektro',
+            fakultas : 'FIKR',
+            singkatan :'TE'
+        },
+        {
+            prodi : 'Manajemen Informatika',
+            fakultas : 'FIKR',
+            singkatan :'MI'
+        },
+        {
+            prodi : 'Manajemen',
+            fakultas : 'FEB',
+            singkatan :'MJ'
+        },
+        {
+            prodi : 'Akuntansi',
+            fakultas : 'FEB',
+            singkatan :'AK'
+        }
+    ];
+
+    res.render('prodi',  {title : 'Halaman Prodi', prodi});
+})
 // handle route yang tidak terdaftar
-app.use('/', (req, res) => {
-    req.send('<h1>404 Not Found</h1>');
+app.use('', (req, res) => {
+    res.send('<h1>404 Not Found</h1>');
 });
 
 // jalankan server
